7ec2b4eeb9e403058955894a91cda6ff4e30550c
WiseVizo <mecoc1011@gmail.com>
2024-01-07 09:32:42 +0530

    enum

---
+++ java12.java
@@ -0,0 +1,16 @@
+
+// enumeration
+
+enum A{
+    run, code, failed, rerun
+}
+
+public class java12 {
+    public static void main(String[] args) {
+        A status = A.run;
+        System.out.println(status);
+        status = A.failed;
+        
+    }
+    
+}
