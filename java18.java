ce0726328f4f30dd0ee0ecdc0c67f48445bb53c6
WiseVizo <mecoc1011@gmail.com>
2024-01-10 09:26:15 +0530

    exception handling

---
+++ java18.java
@@ -0,0 +1,13 @@
+public class java18 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        try{
+            j = 10/i;
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
+        System.out.println("bye");
+    }   
+}
